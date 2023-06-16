0fec4ac97b60b3769c41e72202f3811d279ebe21
Shihabun Mobin Jisan <shihabunjisan@gmail.com>
2023-06-16 20:18:58 +0600

    Finised - Counter JS Projec

---
+++ app.js
@@ -0,0 +1,30 @@
+// set initial value to zero
+let count = 0;
+// Select value and Buttons
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("increase")) {
+      count++;
+    } else if (styles.contains("decrease")) {
+      count--;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+
+    value.textContent = count;
+  });
+});
